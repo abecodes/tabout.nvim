eeb5e6655b9b20ee46c0791f9ea9955bb7c5bc24
zeertzjq <zeertzjq@outlook.com>
2021-08-01 01:54:10 +0800

    handle empty string correctly & allow disabling keybindings (#9)
    Cool, thank you very much 👍

--- lua/tabout.lua
+++ lua/tabout.lua
@@ -12,25 +12,25 @@ local completion_binding = ''
 local completion_binding_back = ''
 
 local enable = function()
-    completion_binding = vim.fn.maparg(config.options.tabkey, 'i')
-    completion_binding_back = vim.fn
-                                  .maparg(config.options.backwards_tabkey, 'i')
-
-    if config.options.completion and completion_binding then
-        if config.debug then
-            logger.log('setting: ' .. config.options.tabkey ..
-                           ':!pumvisible() ? "<Cmd>Tabout<Cr>" : ' ..
-                           completion_binding)
+    if config.options.tabkey ~= '' then
+        completion_binding = vim.fn.maparg(config.options.tabkey, 'i')
+        if config.options.completion and completion_binding ~= '' then
+            if config.debug then
+                logger.log('setting: ' .. config.options.tabkey ..
+                               ':!pumvisible() ? "<Cmd>Tabout<Cr>" : ' ..
+                               completion_binding)
+            end
+            utils.map('i', config.options.tabkey,
+                      '!pumvisible() ? "<Cmd>Tabout<Cr>" : ' .. completion_binding,
+                      {silent = true, expr = true})
+        else
+            utils.map('i', config.options.tabkey, "<Cmd>Tabout<Cr>", {silent = true})
         end
-        utils.map('i', config.options.tabkey,
-                  '!pumvisible() ? "<Cmd>Tabout<Cr>" : ' .. completion_binding,
-                  {silent = true, expr = true})
-    else
-        utils.map('i', config.options.tabkey, "<Cmd>Tabout<Cr>", {silent = true})
     end
 
-    if config.options.enable_backwards then
-        if config.options.completion and completion_binding_back then
+    if config.options.backwards_tabkey ~= '' and config.options.enable_backwards then
+        completion_binding_back = vim.fn.maparg(config.options.backwards_tabkey, 'i')
+        if config.options.completion and completion_binding_back ~= '' then
             if config.debug then
                 logger.log('setting: ' .. config.options.backwards_tabkey ..
                                ':!pumvisible() ? "<Cmd>TaboutBack<Cr>" : ' ..
@@ -51,27 +51,27 @@ end
 
 local disable = function()
     if config.debug then logger.log("unsetting: " .. config.options.tabkey) end
-    utils.unmap('i', config.options.tabkey)
 
-    if config.options.enable_backwards then
+    if config.options.tabkey ~= '' then
+        utils.unmap('i', config.options.tabkey)
+        if config.options.completion and completion_binding ~= '' then
+            if config.debug then
+                logger.log("resetting: " .. config.options.tabkey .. ": " ..
+                               completion_binding)
+            end
+            utils.map('i', config.options.tabkey, completion_binding, {
+                silent = true,
+                expr = string.sub(completion_binding, 1, 2) == 'v:'
+            })
+        end
+    end
+
+    if config.options.backwards_tabkey ~= '' and config.options.enable_backwards then
         if config.debug then
             logger.log("unsetting: " .. config.options.backwards_tabkey)
         end
         utils.unmap('i', config.options.backwards_tabkey)
-    end
-
-    if config.options.completion and completion_binding then
-        if config.debug then
-            logger.log("resetting: " .. config.options.tabkey .. ": " ..
-                           completion_binding)
-        end
-        utils.map('i', config.options.tabkey, completion_binding, {
-            silent = true,
-            expr = string.sub(completion_binding, 1, 2) == 'v:'
-        })
-    end
-    if config.options.enable_backwards then
-        if config.options.completion and completion_binding_back then
+        if config.options.completion and completion_binding_back ~= '' then
             if config.debug then
                 logger.log("resetting: " .. config.options.backwards_tabkey ..
                                ": " .. completion_binding_back)
